1e2626a891dc033e1dff6be3565d3df7200b8205
Sathish <sathishsaravanan2205@gmail.com>
2026-02-16 21:12:08 +0530

    Fix merge conflict in Expense.js

--- server/models/Expense.js
+++ server/models/Expense.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    category: {
-        type: String,
-        required: true,
-<<<<<<< HEAD
-        enum: ['Equipment', 'Travel', 'Editing', 'Staff Payment', 'Rent','Print', 'Marketing', 'Other'],
-=======
-        enum: ['Equipment', 'Travel', 'Editing', 'Staff Payment', 'Rent', 'Print', 'Marketing', 'Other'],
->>>>>>> 41ecd30 (Fix Print expense category issue)
-    },
-    amount: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-    description: { type: String, trim: true, default: '' },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        category: {
+                type: String,
+                required: true,
+                enum: ['Equipment', 'Travel', 'Editing', 'Staff Payment', 'Rent', 'Print', 'Marketing', 'Other'],
+        },
+        amount: { type: Number, required: true },
+        date: { type: Date, default: Date.now },
+        description: { type: String, trim: true, default: '' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Expense', expenseSchema);
